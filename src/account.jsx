d0b2dcece75018317f3227fe7f003a8f1058e2e7
yaredgebre <yaredgebre120@gmail.com>
2025-11-10 09:49:49 -0300

    changing the css of  login

--- src/account.jsx
+++ src/account.jsx
@@ -2,6 +2,7 @@ import "./account.css";
 import React,{useState} from "react";
 import Subject from "./subject";
 
+
 function Access() {
   const [value, setvalue] = useState({username:"", password:""})
   const [newpage, setnewpage] = useState(false)
@@ -26,15 +27,30 @@ function Access() {
     return <Subject/>
   }
   return (
+
     <div>
     <h1 style={{textAlign:"center", position: "relative"}}>smart study</h1>
    <form className="form-container" onSubmit={login}>
-    <h1>Login </h1>
+    <h1>Login Now </h1>
     <input type="text" name="username" onChange={inputs} placeholder="username" value = {value.username}/>
     <br />
     <input type="password" name="password" onChange={inputs} placeholder="password" value = {value.password}/>
     <br />
-    <button type="submit" onClick={login}>Login</button>
+    <label>
+      <button className="login-using-password" type="submit" onClick={login}>Login</button>
+      </label>
+    <label>
+    <button className="login-using-google" type="submit" onClick={login}>
+      <img src="./images/googlesvg.svg" style={{ width: "20px", marginRight: "8px", }} />
+  Login 
+</button>
+
+    </label>
+    <label>
+     <a>Create Account</a>
+    </label>
+   
+  
   </form>
   </div>)};
 
