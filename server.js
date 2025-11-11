be84e63c77c4692187f3b6776a7f070683b85dae
yaredgebre <yaredgebre120@gmail.com>
2025-11-11 14:20:20 -0300

    rewrtting the code

--- server.js
+++ server.js
@@ -8,30 +8,27 @@ const client = new MongoClient(uri);
 app.use(express.json());
 app.use(cors());
 let collection;
- async function run(){
-    try{
-      await client.connect();
-      const database = client.db("mydatabase");
-      collection = database.collection("mycollection");
-      console.log("the data is stored successfully");x
-      app.listen(Port, ()=>{
-  console.log(`listening to port ${Port}`)
-})
-    }
-    catch(error){
-      console.error(error)
-    }
-
+async function run() {
+  try{
+  await client.connect();
+  const database = client.db("mydatabase");
+  collection = database.collection("mycollection");
+  app.listen(Port,()=>{
+    console.log(`Listening to port ${Port}`);
+  })
   }
+  catch(error){
+   console.log("error", error);
+  };
+  
+  
+}
   run();
 
 app.post("/api/data", async(req,res)=>{
   const {title, subject, content} = req.body;
   const inputs  = {title: title, subject: subject, content:content};
   await collection.insertOne(inputs);
- 
-  
-
 
 })
 
