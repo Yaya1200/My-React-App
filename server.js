73750025ac340092d21547027be7bbb171ff2971
yaredgebre <yaredgebre120@gmail.com>
2025-11-11 13:05:20 -0300

    inputing the uri

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from  'express';
+import { MongoClient } from 'mongodb';
 import cors from 'cors';
 const app = express();
 const Port = 5000;
+const uri = "mongodb://localhost:27017";
+const client = MongoClient(uri)
 app.use(express.json());
 app.use(cors());
+
 app.post("/api/data", (req,res)=>{
-  console.log(req.body);
+  const {title, subject, content} = req.body;
+
 })
 
 app.listen(Port, ()=>{
