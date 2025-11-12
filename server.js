3a1956936193658d171e9dc06071ccad47baabe1
yaredgebre <yaredgebre120@gmail.com>
2025-11-12 09:11:45 -0300

    adding the prop to the list

--- server.js
+++ server.js
@@ -45,18 +45,8 @@ app.post("/api/data", async(req,res)=>{
 app.delete("/api/data/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    console.log("Deleting ID:", id); // debug log
-
-    if (!id) {
-      return res.status(400).json({ message: "ID is required" });
-    }
-
+    console.log("Deleting ID:", id); 
     const result = await collection.deleteOne({ _id: new ObjectId(id) });
-
-    if (result.deletedCount === 0) {
-      return res.status(404).json({ message: "Document not found" });
-    }
-
     res.status(200).json({ message: "Deleted successfully" });
   } catch (error) {
     console.error("Error deleting item:", error);
